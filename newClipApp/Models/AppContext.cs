1985b3030c25227307d7cd4d215ae27e932e6c31
wadimj <wadim.jablonski@gmail.com>
2019-05-17 20:13:35 +0200

    Init

---
+++ newClipApp/Models/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace newClipApp.Models
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Clip> Clips { get; set; }
+    }
+}
---
+++ newClipApp/Models/Clip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace newClipApp.Models
+{
+    public class Clip
+    {
+        public int ClipId { get; set; }
+        public string Name { get; set; }
+        public TimeSpan Length { get; set; }
+
+    }
+}
